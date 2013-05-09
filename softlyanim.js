2c0e7b7aea2d5b09daa407f8bf423baa93f3cd9e
Takahiro Morimoto <morimoto@wood-roots.com>
2013-05-09 18:01:09 +0900

    edit

--- softlyanim.js
+++ softlyanim.js
@@ -20,29 +20,34 @@ var settings = $.extend( {
 		'delay': 0 //1アクション後のタメ
 	}, options);
 
-//アニメーション
-var softlyanim = function(obj){
-
-	if(typeof crleft === 'undefined'){
-		crleft = parseInt($(obj).css('left'));
-	}
-	if(typeof crtop === 'undefined'){
-		crtop = parseInt($(obj).css('top'));
-	}
-	
-	$(obj).animate({
-		left: crleft - ((settings.h/2) - Math.floor( Math.random() * (settings.h))) + 'px',
-		top: crtop - ((settings.v/2) - Math.floor( Math.random() * (settings.v))) + 'px'
-		},settings.speed,settings.easing).delay(settings.delay).promise().done(function(){
-			if(settings.repeat){
-				softlyanim(obj);
-			}
-		});
-}
 
 	//処理の実行
 	return this.each(function() {
-		softlyanim(this);
+		var self = $(this);
+		
+		//現在の値を取る
+		if(typeof crleft === 'undefined'){
+			var crleft = parseInt(self.css('left')) || 0;
+		}
+		if(typeof crtop === 'undefined'){
+			var crtop = parseInt(self.css('top')) || 0;
+		}
+
+		//アニメーション定義
+		var softlyanim = function(obj){
+
+			obj.animate({
+				left: crleft - ((settings.h/2) - Math.floor( Math.random() * (settings.h))) + 'px',
+				top: crtop - ((settings.v/2) - Math.floor( Math.random() * (settings.v))) + 'px'
+				},settings.speed,settings.easing).delay(settings.delay).promise().done(function(){
+					if(settings.repeat){
+						softlyanim(obj);
+					}
+				});
+		}
+
+		//実行
+		softlyanim(self);
 	});
 
   };
